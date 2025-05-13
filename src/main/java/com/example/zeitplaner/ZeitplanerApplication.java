1811a19ec521b03b525d815a750d9efb25745b58
okramDE <ole@kram.de>
2025-05-13 21:02:28 +0200

    Init commit

---
+++ src/main/java/com/example/zeitplaner/ZeitplanerApplication.java
@@ -0,0 +1,13 @@
+package com.example.zeitplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeitplanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeitplanerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zeitplaner/ZeitplanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zeitplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeitplanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
